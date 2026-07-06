d61eb80a2943d11557f99fa16794d47ae4d87b9b
Chithusha Hansaka <chithusha05hansaka@gmail.com>
2026-07-07 00:14:48 +0530

    Remove distDir from next.config.ts
    Removed the distDir configuration from nextConfig.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  distDir: "build", // new part
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "unsplash.com" },
